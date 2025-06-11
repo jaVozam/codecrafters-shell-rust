3e84ac10d69836ea93bb70168e5ac9f6beabea58
jaVozam <123635258+jaVozam@users.noreply.github.com>
2025-06-11 03:41:56 +0200

    [any message]

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,10 @@ fn main() {
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
 
+        if input == "exit 0" {
+            break;
+        }
+
         println!("{}: command not found", input.trim());
-
-
     }
 }
