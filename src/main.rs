a8deb7059b829e6588372e3a20f02fbace7c1ce3
jaVozam <123635258+jaVozam@users.noreply.github.com>
2025-06-11 05:47:03 +0200

    [any message]

--- src/main.rs
+++ src/main.rs
@@ -13,19 +13,31 @@ fn main() {
         stdin.read_line(&mut input).unwrap();
 
         let (cmd, args) = match input.trim().split_once(' ') {
-            Some((cmd, args)) => (cmd, args),
-            None => (input.as_str(), ""),
+            Some((cmd, args)) => (cmd, args.split(' ').collect()),
+            None => (input.as_str(), Vec::new()),
         };
 
         match cmd {
             "exit" => {
-                if args == "" {
-                    break;
-                }
-                std::process::exit(args.parse().unwrap());
+                std::process::exit(0);
             }
             "echo" => {
-                println!("{}", args)
+                println!("{}", args.join(" "));
+            }
+            "type" => {
+                let builtin = ["echo", "exit", "type"];
+
+                if args.len() == 0 {
+                    return;
+                }
+
+                for arg in args {
+                    if builtin.contains(&arg) {
+                        println!("{} is a shell builtin", arg);
+                    } else {
+                        println!("{}: not found", arg);
+                    }
+                }
             }
             _ => {
                 println!("{}: command not found", input.trim());
