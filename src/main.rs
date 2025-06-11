63c536eec1fe42a606a4b16b9e8a36f6ede49874
jaVozam <123635258+jaVozam@users.noreply.github.com>
2025-06-11 18:09:31 +0200

    [any message]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,39 @@
+use std::env;
 #[allow(unused_imports)]
 use std::io::{self, Write};
+
+fn cmd_echo(args: &[&str]) {
+    println!("{}", args.join(" "));
+}
+
+fn cmd_type(args: &[&str], builtin: &[&str]) {
+    if args.len() == 0 {
+        return;
+    }
+
+    for arg in args {
+        if builtin.contains(&arg) {
+            println!("{} is a shell builtin", arg);
+            continue;
+        }
+
+        if let Ok(path_var) = env::var("PATH") {
+            let path_entries = env::split_paths(&path_var);
+
+            for dir in path_entries {
+                let full_path = dir.join(&arg);
+                if full_path.exists() {
+                    println!("{} is {}", arg, full_path.display());
+                    return;
+                }
+            }
+        } else {
+            eprintln!("Failed to get PATH variable");
+        }
+
+        println!("{}: not found", arg);
+    }
+}
 
 fn main() {
     loop {
@@ -12,9 +46,11 @@ fn main() {
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
 
+        let builtin = ["echo", "exit", "type"];
+
         let (cmd, args) = match input.trim().split_once(' ') {
             Some((cmd, args)) => (cmd, args.split(' ').collect()),
-            None => (input.as_str(), Vec::new()),
+            None => (input.trim(), Vec::new()),
         };
 
         match cmd {
@@ -22,25 +58,13 @@ fn main() {
                 std::process::exit(0);
             }
             "echo" => {
-                println!("{}", args.join(" "));
+                cmd_echo(&args);
             }
             "type" => {
-                let builtin = ["echo", "exit", "type"];
-
-                if args.len() == 0 {
-                    return;
-                }
-
-                for arg in args {
-                    if builtin.contains(&arg) {
-                        println!("{} is a shell builtin", arg);
-                    } else {
-                        println!("{}: not found", arg);
-                    }
-                }
+                cmd_type(&args, &builtin);
             }
             _ => {
-                println!("{}: command not found", input.trim());
+                println!("{}: command not found", cmd);
             }
         }
     }
