d05b65cd0670ad93721f3cbaf8846dc014dae07c
jaVozam <123635258+jaVozam@users.noreply.github.com>
2025-06-13 04:29:18 +0200

    [any message]

---
+++ src/commands.rs
@@ -0,0 +1,98 @@
+use std::env;
+
+fn cmd_echo(args: &Vec<String>) {
+    println!("{}", args.join(" "));
+}
+
+fn cmd_type(args: &Vec<String>, builtin: &[&str]) {
+    if args.len() == 0 {
+        return;
+    }
+
+    for arg in args {
+        if builtin.contains(&arg.as_str()) {
+            println!("{} is a shell builtin", arg);
+            continue;
+        }
+
+        if let Ok(path_var) = env::var("path") {
+            let path_entries = env::split_paths(&path_var);
+
+            for dir in path_entries {
+                let full_path = dir.join(&arg);
+                if full_path.exists() {
+                    println!("{} is {}", arg, full_path.display());
+                    return;
+                }
+            }
+        } else {
+            eprintln!("failed to get path variable");
+        }
+
+        println!("{}: not found", arg);
+    }
+}
+
+fn cmd_pwd() {
+    println!("{}", env::current_dir().unwrap().display());
+}
+
+fn cmd_cd(args: &Vec<String>) {
+    let args_len = args.len();
+
+    if args_len == 0 {
+        return;
+    }
+    if args_len > 1 {
+        println!("cd: too many arguments");
+        return;
+    }
+    let path = match env::home_dir() {
+        Some(home_path) => {
+            let home_str = home_path.to_str().unwrap_or("");
+            args[0].replace("~", home_str) // returns a new string
+        }
+        None => {
+            println!("could not determine the home directory.");
+            args[0].to_string() // still return something
+        }
+    };
+    let new_dir = std::path::Path::new(&path);
+    match env::set_current_dir(new_dir) {
+        Ok(_) => {}
+        Err(_) => {
+            println!("{}: no such file or directory", path)
+        }
+    };
+}
+
+pub fn command_handler(cmd: &str, args: &Vec<String>, builtin: &[&str]) {
+    match cmd {
+        "exit" => {
+            std::process::exit(0);
+        }
+        "echo" => {
+            cmd_echo(args);
+        }
+        "type" => {
+            cmd_type(args, &builtin);
+        }
+        "pwd" => {
+            cmd_pwd();
+        }
+        "cd" => {
+            cmd_cd(args);
+        }
+        _ => {
+            let mut command = std::process::Command::new(cmd);
+            command.args(args);
+
+            match command.status() {
+                Ok(_) => {}
+                Err(_) => {
+                    println!("{}: command not found", cmd);
+                }
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,103 +1,7 @@
-use std::env;
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
-fn cmd_echo(args: &Vec<String>) {
-    println!("{}", args.join(" "));
-}
-
-fn cmd_type(args: &Vec<String>, builtin: &[&str]) {
-    if args.len() == 0 {
-        return;
-    }
-
-    for arg in args {
-        if builtin.contains(&arg.as_str()) {
-            println!("{} is a shell builtin", arg);
-            continue;
-        }
-
-        if let Ok(path_var) = env::var("PATH") {
-            let path_entries = env::split_paths(&path_var);
-
-            for dir in path_entries {
-                let full_path = dir.join(&arg);
-                if full_path.exists() {
-                    println!("{} is {}", arg, full_path.display());
-                    return;
-                }
-            }
-        } else {
-            eprintln!("Failed to get PATH variable");
-        }
-
-        println!("{}: not found", arg);
-    }
-}
-
-fn cmd_pwd() {
-    println!("{}", env::current_dir().unwrap().display());
-}
-
-fn cmd_cd(args: &Vec<String>) {
-    let args_len = args.len();
-
-    if args_len == 0 {
-        return;
-    }
-    if args_len > 1 {
-        println!("cd: too many arguments");
-        return;
-    }
-    let path = match env::home_dir() {
-        Some(home_path) => {
-            let home_str = home_path.to_str().unwrap_or("");
-            args[0].replace("~", home_str) // Returns a new String
-        }
-        None => {
-            println!("Could not determine the home directory.");
-            args[0].to_string() // Still return something
-        }
-    };
-    let new_dir = std::path::Path::new(&path);
-    match env::set_current_dir(new_dir) {
-        Ok(_) => {}
-        Err(_) => {
-            println!("{}: No such file or directory", path)
-        }
-    };
-}
-
-fn command_handler(cmd: &str, args: &Vec<String>, builtin: &[&str]) {
-    match cmd {
-        "exit" => {
-            std::process::exit(0);
-        }
-        "echo" => {
-            cmd_echo(args);
-        }
-        "type" => {
-            cmd_type(args, &builtin);
-        }
-        "pwd" => {
-            cmd_pwd();
-        }
-        "cd" => {
-            cmd_cd(args);
-        }
-        _ => {
-            let mut command = std::process::Command::new(cmd);
-            command.args(args);
-
-            match command.status() {
-                Ok(_) => {}
-                Err(_) => {
-                    println!("{}: command not found", cmd);
-                }
-            }
-        }
-    }
-}
+mod commands;
 
 fn input() -> String {
     print!("$ ");
@@ -127,14 +31,22 @@ fn input() -> String {
 fn parse_input(input: String) -> (String, Vec<String>) {
     let mut result = Vec::new();
     let mut current = String::new();
-    let mut in_quotes = false;
+    let mut in_s_quotes = false;
+    let mut in_d_quotes = false;
     for char in input.chars() {
         match char {
             '\'' => {
-                in_quotes = !in_quotes;
+                if !in_d_quotes {
+                    in_s_quotes = !in_s_quotes;
+                }
+            }
+            '"' => {
+                if !in_s_quotes {
+                    in_d_quotes = !in_d_quotes;
+                }
             }
             ' ' => {
-                if !in_quotes {
+                if !in_s_quotes && !in_d_quotes {
                     if !current.is_empty() {
                         result.push(current.clone());
                         current.clear();
@@ -170,6 +82,6 @@ fn main() {
 
         let (cmd, args) = parse_input(input);
 
-        command_handler(&cmd, &args, &builtin);
+        commands::command_handler(&cmd, &args, &builtin);
     }
 }
