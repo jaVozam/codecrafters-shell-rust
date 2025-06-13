d29707f69d15ca4bcf89c629e7200809a9db7277
jaVozam <123635258+jaVozam@users.noreply.github.com>
2025-06-13 05:56:55 +0200

    [any message]

--- src/main.rs
+++ src/main.rs
@@ -64,7 +64,7 @@ fn parse_input(input: String) -> (String, Vec<String>) {
                 } else if in_d_quotes && !escape {
                     current.push(char);
                     escape = true;
-                } else if escape {
+                } else if escape && in_d_quotes {
                     escape = false;
                 } else {
                     escape = true;
